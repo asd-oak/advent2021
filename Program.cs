e0844904513320d9a43dc8dd82ac740af4ddd6a9
Andrew Davidson <48331523+asd-oak@users.noreply.github.com>
2021-12-03 00:19:25 -0800

    Finish Day 3

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
+// Navigate to /swagger upon running
+
 
 app.MapGet("/day1-1", () =>
 {
@@ -99,10 +101,74 @@ app.MapGet("/day3-1", () =>
     var mostCommon = oneCounts.Select(count => count > listLength/2 ? "1" : "0");
     var leastCommon = mostCommon.Select(item => item == "1" ? "0" : "1");
 
-    var gamma = Convert.ToInt64(string.Join("", mostCommon), 2);
-    var epsilon = Convert.ToInt64(string.Join("", leastCommon), 2);
+    var gamma = Convert.ToInt32(string.Join("", mostCommon), 2);
+    var epsilon = Convert.ToInt32(string.Join("", leastCommon), 2);
 
     return gamma * epsilon; //3923414
+    
+}
+);
+
+app.MapGet("/day3-2", () =>
+{
+    var filePath = System.IO.Path.Combine(builder.Environment.ContentRootPath, @"input\day3.txt");
+    var binaryStrings = File.ReadLines(filePath).ToList();
+    var bitCount = binaryStrings.First().Length;
+
+    var oxygenBinary = "";
+    var currentList = binaryStrings;
+    for (var i = 0; i < bitCount; i++) {
+        if(!currentList.Any()) {
+            throw new Exception("Uh oh");
+        }
+
+        var listLength = currentList.Count;
+        if(listLength == 1) {
+            oxygenBinary = currentList.Single();
+            break;
+        }
+
+        // For oxygen, keep the rows with most common bit. Tie goes to 1.
+        var onesCount = currentList.Where(item => item[i] == '1').Count();
+        if (onesCount >= listLength / 2.0) {
+            currentList = currentList.Where(item => item[i] == '1').ToList();
+        } else {
+            currentList = currentList.Where(item => item[i] == '0').ToList();
+        }
+    }
+    if(string.IsNullOrWhiteSpace(oxygenBinary)) {
+        oxygenBinary = currentList.Single();
+    }
+
+    var co2Binary = "";
+    currentList = binaryStrings;
+    for (var i = 0; i < bitCount; i++) {
+        if(!currentList.Any()) {
+            throw new Exception("Uh oh");
+        }
+
+        var listLength = currentList.Count;
+        if(listLength == 1) {
+            co2Binary = currentList.Single();
+            break;
+        }
+        
+        //For co2, keep the rows with least common bit. Tie goes to 0.
+        var onesCount = currentList.Where(item => item[i] == '1').Count();
+        if (onesCount > 0 && onesCount < listLength / 2.0) {
+            currentList = currentList.Where(item => item[i] == '1').ToList();
+        } else {
+            currentList = currentList.Where(item => item[i] == '0').ToList();
+        }
+    }
+    if(string.IsNullOrWhiteSpace(co2Binary)) {
+        co2Binary = currentList.Single();
+    }
+
+    var oxygen = Convert.ToInt32(string.Join("", oxygenBinary), 2);
+    var co2 = Convert.ToInt32(string.Join("", co2Binary), 2);
+
+    return oxygen * co2; //No: 5805891. Yes: 5852595
     
 }
 );
