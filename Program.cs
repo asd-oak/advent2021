e81fa0898cccefc521f77923a29c7924261ae7e8
Andrew Davidson <48331523+asd-oak@users.noreply.github.com>
2021-12-04 22:27:49 -0800

    Day 5

--- Program.cs
+++ Program.cs
@@ -288,4 +288,142 @@ app.MapGet("/day4-2", () =>
 }
 );
 
+app.MapGet("/day5-1", () =>
+{
+    var filePath = System.IO.Path.Combine(builder.Environment.ContentRootPath, @"input\day5.txt");
+    var ventVectors = File
+    .ReadLines(filePath)
+    .Select(row => row
+        .Split(" -> ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(coords => coords
+            .Split(",", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)))
+    .ToList();
+
+    var maxGridLength = 0;
+    ventVectors.ForEach(vpair => {
+        var max = Math.Max(vpair.First().Max(), vpair.Last().Max());
+        if(maxGridLength <= max) {
+            maxGridLength = max + 1;
+        }
+    });
+
+    var grid = new int[maxGridLength,maxGridLength];
+    
+    ventVectors.ForEach(vpair => {
+        // Both coordinates match on X
+        if (vpair.First().First() == vpair.Last().First()) {
+            var start = vpair.First().Last();
+            var end = vpair.Last().Last();
+            if (start > end) {
+                end = Interlocked.Exchange(ref start,end);
+            }
+            for (var i = start; i <= end; i++) {
+                grid[vpair.First().First(),i]++;
+            }
+        } else if (vpair.First().Last() == vpair.Last().Last()) {
+            var start = vpair.First().First();
+            var end = vpair.Last().First();
+            if (start > end) {
+                end = Interlocked.Exchange(ref start,end);
+            }
+            for (var i = start; i <= end; i++) {
+                grid[i,vpair.First().Last()]++;
+            }
+        }
+    });
+
+    var twoPlusCount = 0;
+    for (var i = 0; i < maxGridLength; i++) {
+        for (var j = 0; j < maxGridLength; j++) {
+            if(grid[i,j] > 1) {
+                twoPlusCount++;
+            }
+        }
+    }
+    return twoPlusCount; //5167
+
+});
+
+app.MapGet("/day5-2", () =>
+{
+    var filePath = System.IO.Path.Combine(builder.Environment.ContentRootPath, @"input\day5.txt");
+    var ventVectors = File
+    .ReadLines(filePath)
+    .Select(row => row
+        .Split(" -> ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(coords => coords
+            .Split(",", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)))
+    .ToList();
+
+    var maxGridLength = 0;
+    ventVectors.ForEach(vpair => {
+        var max = Math.Max(vpair.First().Max(), vpair.Last().Max());
+        if(maxGridLength <= max) {
+            maxGridLength = max + 1;
+        }
+    });
+
+    var grid = new int[maxGridLength,maxGridLength];
+    
+    ventVectors.ForEach(vpair => {
+        // Both coordinates match on X
+        if (vpair.First().First() == vpair.Last().First()) {
+            var start = vpair.First().Last();
+            var end = vpair.Last().Last();
+            if (start > end) {
+                end = Interlocked.Exchange(ref start,end);
+            }
+            for (var i = start; i <= end; i++) {
+                grid[vpair.First().First(),i]++;
+            }
+        } else if (vpair.First().Last() == vpair.Last().Last()) {
+            //Coordinates match on Y
+            var start = vpair.First().First();
+            var end = vpair.Last().First();
+            if (start > end) {
+                end = Interlocked.Exchange(ref start,end);
+            }
+            for (var i = start; i <= end; i++) {
+                grid[i,vpair.First().Last()]++;
+            }
+        } else {
+            var x1 = vpair.First().First(); 
+            var x2 = vpair.Last().First();
+
+            var y1 = vpair.First().Last();
+            var y2 = vpair.Last().Last();
+
+            if (x1 > x2) {
+              x2 = Interlocked.Exchange(ref x1, x2);
+              y2 = Interlocked.Exchange(ref y1, y2);
+            }
+
+            bool ascending = (y1 < y2);
+
+            var j = y1;
+            for(var i = x1; i <= x2; i++) {
+                grid[i,j]++;
+                if(ascending) {
+                    j++; 
+                } else {
+                    j--;
+                }
+            }
+        };
+    });
+
+    var twoPlusCount = 0;
+    for (var i = 0; i < maxGridLength; i++) {
+        for (var j = 0; j < maxGridLength; j++) {
+            if(grid[i,j] > 1) {
+                twoPlusCount++;
+            }
+        }
+    }
+    return twoPlusCount; //17604
+
+});
+
 app.Run();
