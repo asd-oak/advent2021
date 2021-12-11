c2c280e6d04496d35060efc26bea7272b64f6ecb
Andrew Davidson <48331523+asd-oak@users.noreply.github.com>
2021-12-10 20:54:22 -0800

    Add Honeycomb... BUT injecting a Tracer into a controller to create a span breaks tests. Error: The collection already contains item with same key 'SampleRate'

--- src/advent2021/Program.cs
+++ src/advent2021/Program.cs
@@ -1,10 +1,14 @@
 global using Microsoft.AspNetCore.Mvc;
 global using System.IO;
 
+using Honeycomb.OpenTelemetry;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddHoneycomb(builder.Configuration);
+builder.Services.AddHttpClient();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
