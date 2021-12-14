8d26c44c364a80a31729974fbf1b149af4ec2669
Andrew Davidson <48331523+asd-oak@users.noreply.github.com>
2021-12-13 21:00:17 -0800

    Swagger UI improvement

--- src/advent2021/Program.cs
+++ src/advent2021/Program.cs
@@ -24,7 +24,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => {
+        options.EnableTryItOutByDefault();
+        options.EnableDeepLinking();
+    });
 }
 
 app.UseHttpsRedirection();
