5ceb8d6aa8e8d83acc30de8e49bc5345d53c6d5d
Andrew Davidson <48331523+asd-oak@users.noreply.github.com>
2021-12-20 22:19:17 -0800

    Day 20 - Trench Map

---
+++ src/advent2021/Controllers/Day20.cs
@@ -0,0 +1,186 @@
+using System.Collections;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace advent2021.Controllers;
+
+[ApiController]
+[ApiExplorerSettings(GroupName = "Day 20 - Trench Map")]
+[Route("[controller]")]
+public class Day20Controller : ControllerBase
+{
+
+    public Day20Controller(IWebHostEnvironment environment, Tracer trace)
+    {
+        SampleFilePath = Path.Combine(environment.ContentRootPath, @"input\Day20-sample.txt");
+        FilePath = Path.Combine(environment.ContentRootPath, @"input\Day20.txt");
+        RequestTracer = trace;
+    }
+
+    private string SampleFilePath { get; set; }
+    private string FilePath { get; set; }
+
+    private Tracer RequestTracer { get; set; }
+
+
+    [HttpGet("Part1-Sample")]
+    public long Part1Sample() => SolveBothParts(SampleFilePath, 2);
+
+    [HttpGet("Part1")]
+    public long Part1() => SolveBothParts(FilePath, 2);
+
+    [HttpGet("Part2-Sample")]
+    public long Part2Sample() => SolveBothParts(SampleFilePath, 50);
+
+    [HttpGet("Part2")]
+    public long Part2() => SolveBothParts(FilePath, 50);
+
+    private long SolveBothParts(string filePath, int numIterations)
+    {
+        var enhancementsMap = System.IO.File.ReadLines(filePath).First();
+
+        // Sample and input conveniently flip bit of what to do when a square and it's 8 surrounding are all off (map[0]) or on (map[^1])
+        // Off to off true => empty bit and empty surroundings iterates to empty. On bit and surroundings iterates to on. Stability.
+        // Off to off FALSE => empty bit and empty surroundings iterates to on. On bit and surroundings iterates to off. Unstable.
+        // Off to off FALSE implies implies we'd only ever iterate in pairs if we want to count on bits. Odd iterations to count off bits.
+
+        bool offToOff = enhancementsMap[0] == '.';
+
+        var imageMap = System.IO.File.ReadLines(filePath).Skip(2).ToList();
+
+        using var span = RequestTracer.StartSpan("Preparing");
+        span.SetAttribute("enhancementsLength", enhancementsMap.Length);
+        span.SetAttribute("imageMapRows", imageMap.Count());
+        span.SetAttribute("imageMapCols", imageMap.First().Length);
+
+        var startingGrid = new bool[imageMap.Count(), imageMap.First().Length];
+        foreach (var i in Enumerable.Range(0, imageMap.Count()))
+        {
+            foreach (var j in Enumerable.Range(0, imageMap.First().Length))
+            {
+                if (imageMap[i][j] == '#')
+                {
+                    startingGrid[i, j] = true;
+                }
+            }
+        }
+
+        var updatedGrid = IterateGrid(enhancementsMap, startingGrid, numIterations, true, offToOff);
+
+        var litPixels = CountLitPixesl(updatedGrid);
+        span.SetAttribute("litPixels", litPixels);
+
+        return litPixels; // 5687 too high
+    }
+
+    private bool[,] IterateGrid(string enhancementsMap, bool[,] startingState, int iterationsLeft, bool newGridEmpty, bool isStable)
+    {
+        if (iterationsLeft == 0)
+        {
+            return startingState;
+        }
+        using var span = RequestTracer.StartSpan($"IterateGrid-{iterationsLeft}");
+        span.SetAttribute("litPixelsInput", CountLitPixesl(startingState));
+        span.SetAttribute("newGridEmpty", newGridEmpty);
+
+        //New grid for results, padded once for the space we may grow into and again to bring in some of the infinite canvas
+
+        var updatedGrid = PadTrimGrid(startingState, 2, newGridEmpty);
+        var paddedStartingGrid = (bool[,])updatedGrid.Clone();
+
+        foreach (var i in Enumerable.Range(1, paddedStartingGrid.GetLength(0) - 2))
+        {
+            foreach (var j in Enumerable.Range(1, paddedStartingGrid.GetLength(1) - 2))
+            {
+                var examineBits = new BitArray(32);
+                var index = 8;
+                foreach (var peekI in Enumerable.Range(i - 1, 3))
+                {
+                    foreach (var peekJ in Enumerable.Range(j - 1, 3))
+                    {
+                        if (paddedStartingGrid[peekI, peekJ])
+                        {
+                            examineBits[index] = true;
+                        }
+                        index--;
+                    }
+                }
+                var toInt = new int[1];
+                examineBits.CopyTo(toInt, 0);
+                var mapIndex = toInt[0];
+
+                updatedGrid[i, j] = (enhancementsMap[mapIndex] == '#');
+            }
+        }
+
+        //Trim the outer row of infinite canvas, since we didn't update it
+        updatedGrid = PadTrimGrid(updatedGrid, -1, newGridEmpty);
+
+        span.SetAttribute("litPixels", CountLitPixesl(updatedGrid));
+        span.SetAttribute("litPixelsReference", CountLitPixesl(paddedStartingGrid));
+
+        return IterateGrid(enhancementsMap, updatedGrid, iterationsLeft - 1, isStable ? newGridEmpty : !newGridEmpty, isStable);
+    }
+
+    private bool[,] PadTrimGrid(bool[,] grid, int distance, bool newGridEmpty)
+    {
+        if (distance == 0)
+        {
+            return (bool[,])grid.Clone();
+        }
+        using var span = RequestTracer.StartSpan($"PadTrimGrid-{distance}");
+
+        var doubleDistance = distance * 2;
+        var newGrid = new bool[grid.GetLength(0) + doubleDistance, grid.GetLength(1) + doubleDistance];
+
+        //When expanding, new space defaults to false. Initialize to true if needed.
+        if (!newGridEmpty && distance > 0)
+        {
+            var litCount = 0;
+            span.SetAttribute("expectedInitialization", 2 * distance * (newGrid.GetLength(0) + newGrid.GetLength(1)) - 4 * Math.Pow(distance, 2));
+            foreach (var i in Enumerable.Range(0, newGrid.GetLength(0)))
+            {
+                foreach (var j in Enumerable.Range(0, newGrid.GetLength(1)))
+                {
+                    if (i < distance || j < distance || newGrid.GetLength(0) - i <= distance || newGrid.GetLength(1) - j <= distance)
+                    {
+                        newGrid[i, j] = true;
+                        litCount++;
+                    }
+                }
+            }
+            span.SetAttribute("observedInitialization", litCount);
+        }
+
+        //Expanding or shrinking, copy only the retained subgrid
+        //Expanding, copy entire grid into new grid, offset by distance
+        //Shrinking, offset into current grid to copy to new grid
+        foreach (var i in Enumerable.Range(0, grid.GetLength(0)))
+        {
+            foreach (var j in Enumerable.Range(0, grid.GetLength(1)))
+            {
+                if (i + distance >= 0 && j + distance >= 0 && i + distance < newGrid.GetLength(0) && j + distance < newGrid.GetLength(1))
+                {
+                    newGrid[i + distance, j + distance] = grid[i, j];
+                }
+            }
+        }
+        return newGrid;
+    }
+
+    private int CountLitPixesl(bool[,] grid)
+    {
+        var litPixels = 0;
+        foreach (var i in Enumerable.Range(0, grid.GetLength(0)))
+        {
+            foreach (var j in Enumerable.Range(0, grid.GetLength(1)))
+            {
+                if (grid[i, j])
+                {
+                    litPixels++;
+                }
+            }
+        }
+        return litPixels;
+    }
+}
--- test/advent2021.Tests/AllTests.cs
+++ test/advent2021.Tests/AllTests.cs
@@ -8,7 +8,8 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _factory;
 
-    public enum Puzzle {
+    public enum Puzzle
+    {
         Part1Sample,
         Part1Sample2,
         Part1Sample3,
@@ -19,7 +20,7 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
         Part2
     };
 
-    public Dictionary<Puzzle,string> _queryParts = new Dictionary<Puzzle, string>() {
+    public Dictionary<Puzzle, string> _queryParts = new Dictionary<Puzzle, string>() {
         {Puzzle.Part1Sample, "Part1-Sample"},
         {Puzzle.Part1Sample2, "Part1-Sample2"},
         {Puzzle.Part1Sample3, "Part1-Sample3"},
@@ -36,10 +37,10 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"7")]
-    [InlineData(Puzzle.Part1,"1215")]
-    [InlineData(Puzzle.Part2Sample,"5")]
-    [InlineData(Puzzle.Part2,"1150")]
+    [InlineData(Puzzle.Part1Sample, "7")]
+    [InlineData(Puzzle.Part1, "1215")]
+    [InlineData(Puzzle.Part2Sample, "5")]
+    [InlineData(Puzzle.Part2, "1150")]
     public async void TestDay1SonarSweepAsync(Puzzle part, string answer)
     {
         var client = _factory.CreateClient();
@@ -50,10 +51,10 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"150")]
-    [InlineData(Puzzle.Part1,"1693300")]
-    [InlineData(Puzzle.Part2Sample,"900")]
-    [InlineData(Puzzle.Part2,"1857958050")]
+    [InlineData(Puzzle.Part1Sample, "150")]
+    [InlineData(Puzzle.Part1, "1693300")]
+    [InlineData(Puzzle.Part2Sample, "900")]
+    [InlineData(Puzzle.Part2, "1857958050")]
     public async void TestDay2DiveAsync(Puzzle part, string answer)
     {
         var client = _factory.CreateClient();
@@ -64,10 +65,10 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"198")]
-    [InlineData(Puzzle.Part1,"3923414")]
-    [InlineData(Puzzle.Part2Sample,"230")]
-    [InlineData(Puzzle.Part2,"5852595")]
+    [InlineData(Puzzle.Part1Sample, "198")]
+    [InlineData(Puzzle.Part1, "3923414")]
+    [InlineData(Puzzle.Part2Sample, "230")]
+    [InlineData(Puzzle.Part2, "5852595")]
     public async void TestDay3BinaryDiagnosticAsync(Puzzle part, string answer)
     {
         var client = _factory.CreateClient();
@@ -78,10 +79,10 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"4512")]
-    [InlineData(Puzzle.Part1,"34506")]
-    [InlineData(Puzzle.Part2Sample,"1924")]
-    [InlineData(Puzzle.Part2,"7686")]
+    [InlineData(Puzzle.Part1Sample, "4512")]
+    [InlineData(Puzzle.Part1, "34506")]
+    [InlineData(Puzzle.Part2Sample, "1924")]
+    [InlineData(Puzzle.Part2, "7686")]
     public async void TestDay4GiantSquidAsync(Puzzle part, string answer)
     {
         var client = _factory.CreateClient();
@@ -92,10 +93,10 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"5")]
-    [InlineData(Puzzle.Part1,"5167")]
-    [InlineData(Puzzle.Part2Sample,"12")]
-    [InlineData(Puzzle.Part2,"17604")]
+    [InlineData(Puzzle.Part1Sample, "5")]
+    [InlineData(Puzzle.Part1, "5167")]
+    [InlineData(Puzzle.Part2Sample, "12")]
+    [InlineData(Puzzle.Part2, "17604")]
     public async void TestDay5HydrothermalVentureAsync(Puzzle part, string answer)
     {
         var client = _factory.CreateClient();
@@ -106,10 +107,10 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"5934")]
-    [InlineData(Puzzle.Part1,"360761")]
-    [InlineData(Puzzle.Part2Sample,"26984457539")]
-    [InlineData(Puzzle.Part2,"1632779838045")]
+    [InlineData(Puzzle.Part1Sample, "5934")]
+    [InlineData(Puzzle.Part1, "360761")]
+    [InlineData(Puzzle.Part2Sample, "26984457539")]
+    [InlineData(Puzzle.Part2, "1632779838045")]
     public async void TestDay6LanternfishAsync(Puzzle part, string answer)
     {
         var client = _factory.CreateClient();
@@ -120,10 +121,10 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"37")]
-    [InlineData(Puzzle.Part1,"345197")]
-    [InlineData(Puzzle.Part2Sample,"168")]
-    [InlineData(Puzzle.Part2,"96361606")]
+    [InlineData(Puzzle.Part1Sample, "37")]
+    [InlineData(Puzzle.Part1, "345197")]
+    [InlineData(Puzzle.Part2Sample, "168")]
+    [InlineData(Puzzle.Part2, "96361606")]
     public async void TestDay7TheTreacheryOfWhalesAsync(Puzzle part, string answer)
     {
         var client = _factory.CreateClient();
@@ -134,10 +135,10 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"26")]
-    [InlineData(Puzzle.Part1,"543")]
-    [InlineData(Puzzle.Part2Sample,"61229")]
-    [InlineData(Puzzle.Part2,"994266")]
+    [InlineData(Puzzle.Part1Sample, "26")]
+    [InlineData(Puzzle.Part1, "543")]
+    [InlineData(Puzzle.Part2Sample, "61229")]
+    [InlineData(Puzzle.Part2, "994266")]
     public async void TestDay8SevenSegmentSearchAsync(Puzzle part, string answer)
     {
         var client = _factory.CreateClient();
@@ -148,10 +149,10 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"15")]
-    [InlineData(Puzzle.Part1,"631")]
-    [InlineData(Puzzle.Part2Sample,"1134")]
-    [InlineData(Puzzle.Part2,"821560")]
+    [InlineData(Puzzle.Part1Sample, "15")]
+    [InlineData(Puzzle.Part1, "631")]
+    [InlineData(Puzzle.Part2Sample, "1134")]
+    [InlineData(Puzzle.Part2, "821560")]
     public async void TestDay9SmokeBasinAsync(Puzzle part, string answer)
     {
         var client = _factory.CreateClient();
@@ -162,10 +163,10 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"26397")]
-    [InlineData(Puzzle.Part1,"389589")]
-    [InlineData(Puzzle.Part2Sample,"288957")]
-    [InlineData(Puzzle.Part2,"1190420163")]
+    [InlineData(Puzzle.Part1Sample, "26397")]
+    [InlineData(Puzzle.Part1, "389589")]
+    [InlineData(Puzzle.Part2Sample, "288957")]
+    [InlineData(Puzzle.Part2, "1190420163")]
     public async void TestDay10SyntaxScoringAsync(Puzzle part, string answer)
     {
         var client = _factory.CreateClient();
@@ -176,10 +177,10 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"1656")]
-    [InlineData(Puzzle.Part1,"1642")]
-    [InlineData(Puzzle.Part2Sample,"195")]
-    [InlineData(Puzzle.Part2,"320")]
+    [InlineData(Puzzle.Part1Sample, "1656")]
+    [InlineData(Puzzle.Part1, "1642")]
+    [InlineData(Puzzle.Part2Sample, "195")]
+    [InlineData(Puzzle.Part2, "320")]
     public async void TestDay11DumboOctopusAsync(Puzzle part, string answer)
     {
         var client = _factory.CreateClient();
@@ -190,14 +191,14 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"10")]
-    [InlineData(Puzzle.Part1Sample2,"19")]
-    [InlineData(Puzzle.Part1Sample3,"226")]
-    [InlineData(Puzzle.Part1,"5756")]
-    [InlineData(Puzzle.Part2Sample,"36")]
-    [InlineData(Puzzle.Part2Sample2,"103")]
-    [InlineData(Puzzle.Part2Sample3,"3509")]
-    [InlineData(Puzzle.Part2,"144603")]
+    [InlineData(Puzzle.Part1Sample, "10")]
+    [InlineData(Puzzle.Part1Sample2, "19")]
+    [InlineData(Puzzle.Part1Sample3, "226")]
+    [InlineData(Puzzle.Part1, "5756")]
+    [InlineData(Puzzle.Part2Sample, "36")]
+    [InlineData(Puzzle.Part2Sample2, "103")]
+    [InlineData(Puzzle.Part2Sample3, "3509")]
+    [InlineData(Puzzle.Part2, "144603")]
     public async void TestDay12PassagePathingAsync(Puzzle part, string answer)
     {
         var client = _factory.CreateClient();
@@ -208,8 +209,8 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"17")]
-    [InlineData(Puzzle.Part1,"666")]
+    [InlineData(Puzzle.Part1Sample, "17")]
+    [InlineData(Puzzle.Part1, "666")]
     // No part 2 tests - return bitmap for user to view
     public async void TestDay13TransparentOrigamiAsync(Puzzle part, string answer)
     {
@@ -221,10 +222,10 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory]
-    [InlineData(Puzzle.Part1Sample,"1588")]
-    [InlineData(Puzzle.Part1,"2375")]
-    [InlineData(Puzzle.Part2Sample,"2188189693529")]
-    [InlineData(Puzzle.Part2,"1976896901756")]
+    [InlineData(Puzzle.Part1Sample, "1588")]
+    [InlineData(Puzzle.Part1, "2375")]
+    [InlineData(Puzzle.Part2Sample, "2188189693529")]
+    [InlineData(Puzzle.Part2, "1976896901756")]
     public async void TestDay14ExtendedPolymerizationAsync(Puzzle part, string answer)
     {
         var client = _factory.CreateClient();
@@ -233,4 +234,20 @@ public class AllTests : IClassFixture<WebApplicationFactory<Program>>
 
         Assert.Equal(answer, responseContent);
     }
+
+    [Theory]
+    [InlineData(Puzzle.Part1Sample, "35")]
+    [InlineData(Puzzle.Part1, "5597")]
+    [InlineData(Puzzle.Part2Sample, "3351")]
+    [InlineData(Puzzle.Part2, "18723")]
+    public async void TestDay20TrenchMapAsync(Puzzle part, string answer)
+    {
+        var client = _factory.CreateClient();
+        var response = await client.GetAsync($"/Day20/{_queryParts[part]}");
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal(answer, responseContent);
+    }
+
+
 }
